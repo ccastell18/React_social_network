8553de66bdee9beed3eb9893a3ae3cfe112075f8
Christopher Castellano <c.castellano18@gmail.com>
2018-09-03 22:33:18 -0500

    registration success

--- routes/api/users.js
+++ routes/api/users.js
@@ -51,4 +51,26 @@ router.post('/register', (req, res) => {
   });
 });
 
+//@route   GET api/users/login
+//@desc    Login user/ Return JWT token
+//@access  public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //find user by email
+  User.findOne({ email }).then(user => {
+    //check for user
+    if (!user) {
+      return res.status(404).json({ email: 'User not found' });
+    } //check password. unhash password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: 'Success' });
+      } else {
+        return res.status(400).json({ password: 'Password incorrect' });
+      }
+    });
+  });
+});
 module.exports = router;
