47f7fc1bda442eaa01d755fc0cd53ac59576bd5e
Christopher Castellano <c.castellano18@gmail.com>
2018-09-04 18:24:20 -0500

    token made successfully

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://Chris:Tobie1@ds143932.mlab.com:43932/devconnector18'
+  mongoURI: 'mongodb://Chris:Tobie1@ds143932.mlab.com:43932/devconnector18',
+  secretOrKey: 'secret'
 };
