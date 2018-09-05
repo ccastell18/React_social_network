5cdab39893436eaf1fb9b4fd4c6d22e6403cbe5e
Christopher Castellano <c.castellano18@gmail.com>
2018-09-04 22:52:45 -0500

    profile schema

---
+++ models/Profile.js
@@ -0,0 +1,126 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//create Schema
+const ProfileSchema = new Schema({
+  user: {
+    //Schema.Types.ObjectId will identify user by id
+    type: Schema.Types.ObjectId,
+    //ref refers to collection, which only users is available.(from User.js)
+    ref: 'users'
+  },
+  //SEO friendly URL
+  handle: {
+    type: String,
+    required: true,
+    max: 40
+  },
+  company: {
+    type: String
+  },
+  website: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  skills: {
+    //skills will be an array of strings
+    type: [String],
+    required: true
+  },
+  bio: {
+    type: String
+  },
+  githubusername: {
+    type: String
+  },
+  experience: [
+    {
+      title: {
+        type: String,
+        required: true
+      },
+
+      company: {
+        type: String,
+        requird: true
+      },
+      location: {
+        type: String
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        required: true
+      },
+
+      degree: {
+        type: String,
+        requird: true
+      },
+      fieldofstudy: {
+        type: String,
+        required: true
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  social: {
+    youtube: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    },
+    instagram: {
+      type: String
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
