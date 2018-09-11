10b7501aa3b7f6729911dc57b72ced4c557b0f07
Christopher Castellano <c.castellano18@gmail.com>
2018-09-11 15:23:49 -0500

    add experience

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,7 @@ import Landing from './components/layout/Landing';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/createProfile/CreateProfile';
 import EditProfile from './components/edit-profile/EditProfile';
+import AddExperience from './components/add-credentials/AddExperience';
 
 import './App.css';
 import { clearCurrentProfile } from './actions/profileActions';
@@ -67,6 +68,13 @@ class App extends Component {
                   exact
                   path="/edit-profile"
                   component={EditProfile}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/add-experience"
+                  component={AddExperience}
                 />
               </Switch>
             </div>
