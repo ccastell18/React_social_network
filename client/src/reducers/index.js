f17b2b5e8ce097c6df5ceec324c1903ccdcbe975
Christopher Castellano <c.castellano18@gmail.com>
2018-09-12 17:01:36 -0500

    Post Reducer setup

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import errorReducer from './errorReducer';
 import profileReducer from './profileReducer';
+import postReducer from './postReducer';
 
 //put all reducers within curly braces
 export default combineReducers({
   auth: authReducer,
   errors: errorReducer,
-  profile: profileReducer
+  profile: profileReducer,
+  post: postReducer
 });
